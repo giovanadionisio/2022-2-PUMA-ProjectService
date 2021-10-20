0bcc5fcc1f5031a44aff9d374c62f6e3f254aa9a
Gabriel Tiveron <gabrielmtiveron@gmail.com>
2021-10-20 02:14:22 -0300

    [#26] Adding phone number to request response

--- src/routes/ProjectRoutes.js
+++ src/routes/ProjectRoutes.js
@@ -9,7 +9,7 @@ routes.get('/alocated/:subjectId', (req, res) => {
   const subjectId = parseInt(req.params.subjectId)
 
   if(functions.checkInt(subjectId)){
-    db.query("SELECT p.projectId, p.name FROM PROJECT p\
+    db.query("SELECT p.projectId, p.name, p.expectedResult FROM PROJECT p\
               WHERE p.subjectId = $1 AND p.status = 'Aguardando aprovacao';",
               [subjectId]).then(response => {
                 res.status(200).json(response.rows)
@@ -42,12 +42,11 @@ routes.get('/project/:projectId', (req, res) => {
   const projectId = parseInt(req.params.projectId)
 
   if(functions.checkInt(projectId)){
-    db.query("SELECT p.name, p.problem, p.expectedResult, u.fullName\
+    db.query("SELECT p.name, p.problem, p.expectedResult, u.fullName, u.phoneNumber\
               FROM PROJECT p\
                 INNER JOIN COMMON_USER u ON u.userId = p.userId\
                 WHERE p.projectId = $1;",
               [projectId]).then(response => {
-                console.log(response)
                 res.json(response.rows[0])
               })
   }
