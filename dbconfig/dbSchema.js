93f1a153ae998465a388864cede784c1eb9bd4df
Mexazonic <brunnohenriq697@gmail.com>
2022-03-25 13:07:55 -0300

    Adição e Refatoração do Banco

--- dbconfig/dbSchema.js
+++ dbconfig/dbSchema.js
@@ -1,7 +1,7 @@
 /*
   Banco de dados - PUMA
 
-  Nmr Tabelas: 13
+  Nmr Tabelas: 20
   Nome do banco de dados: puma
   Criado por: Gabriel Tiveron
   Última alteração: Criação do script
@@ -11,177 +11,234 @@
 */
 
 module.exports = Object.freeze({
-  DBSCHEMALEN: 15,
+  DBSCHEMALEN: 20,
   DBSCHEMA: `
-  CREATE TYPE stats AS ENUM ('Em alocacao', 'Aguardando aprovacao', 'Aprovado', 'Recusado');
-  CREATE TABLE COMMON_USER (
-      userId SERIAL,
-      fullName VARCHAR(60) NOT NULL,
-      email VARCHAR(70) NOT NULL,
-      passwordHash VARCHAR(1000) NOT NULL,
-      phoneNumber VARCHAR(12),
-      isAdmin BOOL DEFAULT FALSE NOT NULL,
+      CREATE TABLE COMMON_USER (
+        userId SERIAL,
+        fullName VARCHAR(200) NOT NULL,
+        email VARCHAR(70) NOT NULL,
+        isAdmin BOOL DEFAULT FALSE NOT NULL,
+        phoneNumber VARCHAR(12),
+        passwordHash VARCHAR(1000) NOT NULL,
+        
+        CONSTRAINT COMMON_USER_PK PRIMARY KEY (userId),
+        CONSTRAINT COMMON_USER_UK UNIQUE (email)
+    );
 
-      CONSTRAINT COMMON_USER_PK PRIMARY KEY (userId),
-      CONSTRAINT COMMON_USER_UK UNIQUE (email)
-  );
+    CREATE TABLE STUDENT (
+        regNumber VARCHAR(9) NOT NULL,
+        userId SERIAL NOT NULL,
+        softSkills VARCHAR(100) NOT NULL,
 
-  CREATE TABLE STUDENT (
-      regNumber VARCHAR(9) NOT NULL,
-      softSkills VARCHAR(100) NOT NULL,
-      userId SERIAL NOT NULL,
+        CONSTRAINT STUDENT_PK PRIMARY KEY (regNumber),
+        CONSTRAINT STUDENT_UK UNIQUE (userId),
+        CONSTRAINT STUDENT_COMMON_USER_FK FOREIGN KEY (userId)
+          REFERENCES COMMON_USER (userId)
+    );
 
-      CONSTRAINT STUDENT_PK PRIMARY KEY (regNumber),
-      CONSTRAINT STUDENT_UK UNIQUE (userId),
-      CONSTRAINT STUDENT_COMMON_USER_FK FOREIGN KEY (userId)
-        REFERENCES COMMON_USER (userId)
-  );
+    CREATE TABLE JURIDICAL_AGENT (
+        userId SERIAL NOT NULL,
+        cnpj VARCHAR(14) NOT NULL,
+        companyName varchar(100) NOT NULL,
+        socialReason VARCHAR(100) NOT NULL,
+        
+        CONSTRAINT JURIDICAL_AGENT_PK PRIMARY KEY(userId),
+        CONSTRAINT JURIDICAL_AGENT_UK UNIQUE (cnpj),
+        CONSTRAINT JURIDICAL_AGENT_COMMON_USER_FK FOREIGN KEY (userId)
+          REFERENCES COMMON_USER (userId)
+    );
 
-  CREATE TABLE JURIDICAL_AGENT (
-      cnpj VARCHAR(14) NOT NULL,
-      cep VARCHAR(8) NOT NULL,
-      companyName varchar(100) NOT NULL,
-      socialReason VARCHAR(100) NOT NULL,
-      userId SERIAL NOT NULL,
+    CREATE TABLE PHYSICAL_AGENT (
+        userId SERIAL NOT NULL,
+        cpf VARCHAR(11) NOT NULL,
 
-      CONSTRAINT JURIDICAL_AGENT_PK PRIMARY KEY(userId),
-      CONSTRAINT JURIDICAL_AGENT_UK UNIQUE (cnpj),
-      CONSTRAINT JURIDICAL_AGENT_COMMON_USER_FK FOREIGN KEY (userId)
-        REFERENCES COMMON_USER (userId)
-  );
+        CONSTRAINT PHYSICAL_AGENT_PK PRIMARY KEY(userId),
+        CONSTRAINT PHYSICAL_AGENT_UK UNIQUE (cpf),
+        CONSTRAINT PHYSICAL_AGENT_COMMON_USER_FK FOREIGN KEY (userId)
+          REFERENCES COMMON_USER (userId)
+    );
 
-  CREATE TABLE PROFESSOR (
-      regNumber SERIAL,
-      userId SERIAL NOT NULL,
+    CREATE TABLE PROFESSOR (
+        regNumber VARCHAR(20),
+        userId SERIAL NOT NULL,
 
-      CONSTRAINT PROFESSOR_PK PRIMARY KEY (regNumber),
-      CONSTRAINT PROFESSOR_UK UNIQUE (userId),
-      CONSTRAINT PROFESSOR_COMMON_USER_FK FOREIGN KEY (userId)
-        REFERENCES COMMON_USER (userId)
-  );
+        CONSTRAINT PROFESSOR_PK PRIMARY KEY (regNumber),
+        CONSTRAINT PROFESSOR_UK UNIQUE (userId),
+        CONSTRAINT PROFESSOR_COMMON_USER_FK FOREIGN KEY (userId)
+          REFERENCES COMMON_USER (userId)
+    );
 
-  CREATE TABLE PHYSICAL_AGENT (
-      cpf VARCHAR(11) NOT NULL,
-      userId SERIAL NOT NULL,
+    CREATE TABLE SUBJECT (
+        subjectId SERIAL,
+        name VARCHAR(200) NOT NULL,
+        courseSyllabus VARCHAR(10000),
 
-      CONSTRAINT PHYSICAL_AGENT_PK PRIMARY KEY(userId),
-      CONSTRAINT PHYSICAL_AGENT_UK UNIQUE (cpf),
-      CONSTRAINT PHYSICAL_AGENT_COMMON_USER_FK FOREIGN KEY (userId)
-        REFERENCES COMMON_USER (userId)
-  );
+        CONSTRAINT SUBJECT_PK PRIMARY KEY (subjectId)
+    );
 
-  CREATE TABLE SUBJECT (
-      subjectId SERIAL,
-      name VARCHAR(100) NOT NULL,
-      courseSyllabus VARCHAR(10000),
+    CREATE TYPE stats_project AS ENUM ('SB', 'AN', 'AC', 'RC', 'DT', 'EX', 'EC');
+    CREATE TABLE PROJECT (
+        projectId SERIAL,
+        userId SERIAL NOT NULL,
+        subjectId SERIAL,
+        name varchar(100) NOT NULL,
+        expectedResult VARCHAR(500) NOT NULL,
+        problem VARCHAR(1000) NOT NULL,
+        status stats_project DEFAULT 'SB' NOT NULL,
+        createdAt DATE NOT NULL,
+        
 
-      CONSTRAINT SUBJECT_PK PRIMARY KEY (subjectId)
-  );
+        CONSTRAINT PROJECT_PK PRIMARY KEY (projectId),
+        CONSTRAINT PROJECT_COMMON_USER_FK FOREIGN KEY (userId)
+          REFERENCES COMMON_USER (userId),
+        CONSTRAINT PROJECT_SUBJECT_FK FOREIGN KEY (subjectId)
+          REFERENCES SUBJECT (subjectId)
+    );
 
-  CREATE TABLE PROJECT (
+    CREATE TABLE FILE (
+      fileId SERIAL NOT NULL,
       projectId SERIAL,
-      name varchar(50) NOT NULL,
-      problem VARCHAR(100) NOT NULL,
-      expectedResult VARCHAR(500) NOT NULL,
-      status stats DEFAULT 'Em alocacao' NOT NULL,
-      userId SERIAL NOT NULL,
-      subjectId SERIAL,
-
-      CONSTRAINT PROJECT_PK PRIMARY KEY (projectId),
-      CONSTRAINT PROJECT_COMMON_USER_FK FOREIGN KEY (userId)
-        REFERENCES COMMON_USER (userId),
-      CONSTRAINT PROJECT_SUBJECT_FK FOREIGN KEY (subjectId)
-        REFERENCES SUBJECT (subjectId)
-  );
-
-  CREATE TABLE KNOWLEDGE_AREA (
-      knowledgeAreaId SERIAL,
-      knowledgeArea VARCHAR(100) NOT NULL,
-
-      CONSTRAINT KNOWLEDGE_AREA_PK PRIMARY KEY (knowledgeAreaId),
-      CONSTRAINT KNOWLEDGE_AREA_UK UNIQUE (knowledgeArea)
-  );
-
-  CREATE TABLE SUBAREA (
-    subAreaId SERIAL,
-    knowledgeAreaId SERIAL NOT NULL,
-    description VARCHAR(100) NOT NULL,
-
-    CONSTRAINT SUBAREA_PK PRIMARY KEY (subAreaId),
-    CONSTRAINT SUBAREA_KNOWLEDGE_AREA_FK FOREIGN KEY (knowledgeAreaId)
-      REFERENCES KNOWLEDGE_AREA (knowledgeAreaId),
-    CONSTRAINT SUBAREA_UK UNIQUE (description, knowledgeAreaId)
-  );
-
-  CREATE TABLE has (
-      subAreaId SERIAL NOT NULL,
-      projectId SERIAL NOT NULL,
-
-      CONSTRAINT has_SUBAREA_FK FOREIGN KEY (subAreaId)
-        REFERENCES SUBAREA (subAreaId),
-      CONSTRAINT has_PROJECT FOREIGN KEY (projectId)
-        REFERENCES PROJECT (projectId),
-      CONSTRAINT has_UK UNIQUE (subAreaId, projectId)
-  );
-
-  CREATE TABLE FILE (
-      fileId SERIAL,
       fileName VARCHAR(100) NOT NULL,
       byteContent BYTEA NOT NULL,
-      projectId SERIAL,
 
       CONSTRAINT FILE_PK PRIMARY KEY (fileId),
       CONSTRAINT FILE_PROJECT_FK FOREIGN KEY (projectId)
         REFERENCES PROJECT (projectId)
-  );
+    );
 
 
-  CREATE TABLE CLASS (
-      classId SERIAL,
-      subjectTerm VARCHAR(100) NOT NULL,
-      code VARCHAR(3) NOT NULL,
-      studentRegNumber VARCHAR(9),
-      professorRegNumber SERIAL,
-      subjectId SERIAL,
+    CREATE TABLE KEYWORD(
+    keywordId SERIAL NOT NULL,
+    keyword VARCHAR(100) NOT NULL,
 
-      CONSTRAINT CLASS_PK PRIMARY KEY(classId),
-      CONSTRAINT CLASS_PROFESSOR_FK FOREIGN KEY(professorRegNumber)
-        REFERENCES PROFESSOR (regNumber),
-      CONSTRAINT CLASS_STUDENT_FK FOREIGN KEY (studentRegNumber)
-        REFERENCES STUDENT (regNumber),
-      CONSTRAINT CLASS_SUBJECT_FK FOREIGN KEY (subjectId)
-        REFERENCES SUBJECT (subjectId)
-  );
+    CONSTRAINT KEYWORD_PK PRIMARY KEY (keywordId)
+    );
 
-  CREATE TABLE participates (
-      regNumber VARCHAR(9) NOT NULL,
-      classId SERIAL,
+    CREATE TABLE has(
+      keywordId SERIAL NOT NULL,
+      projectId SERIAL NOT NULL,
 
-      CONSTRAINT participates_STUDENT_FK FOREIGN KEY (regNumber)
+      CONSTRAINT has_KEYWORD_FK FOREIGN KEY (keywordId) 
+        REFERENCES KEYWORD(keywordId),
+      CONSTRAINT has_PROJECT_FK FOREIGN KEY (projectId) 
+        REFERENCES PROJECT(projectId)
+        
+    );
+
+    CREATE TABLE resume(
+      keywordId SERIAL NOT NULL,
+      subjectId SERIAL NOT NULL,
+
+      CONSTRAINT resume_KEYWORD_FK FOREIGN KEY (keywordId) 
+        REFERENCES KEYWORD(keywordId),
+      CONSTRAINT has_SUBJECT_FK FOREIGN KEY (subjectId) 
+        REFERENCES SUBJECT(subjectId)
+        
+    );
+
+
+
+    CREATE TABLE KNOWLEDGE_AREA (
+        knowledgeAreaId SERIAL NOT NULL,
+        knowledgeArea VARCHAR(200) NOT NULL,
+
+        CONSTRAINT KNOWLEDGE_AREA_PK PRIMARY KEY (knowledgeAreaId),
+        CONSTRAINT KNOWLEDGE_AREA_UK UNIQUE (knowledgeArea)
+    );
+
+    CREATE TABLE SUBAREA (
+      subAreaId SERIAL NOT NULL,
+      knowledgeAreaId SERIAL NOT NULL,
+      description VARCHAR(200) NOT NULL,
+
+      CONSTRAINT SUBAREA_PK PRIMARY KEY (subAreaId),
+      CONSTRAINT SUBAREA_KNOWLEDGE_AREA_FK FOREIGN KEY (knowledgeAreaId)
+        REFERENCES KNOWLEDGE_AREA (knowledgeAreaId),
+      CONSTRAINT SUBAREA_UK UNIQUE (description, knowledgeAreaId)
+    );
+
+    CREATE TABLE identifies(
+        subAreaId SERIAL NOT NULL,
+        subjectId SERIAL NOT NULL,
+
+        CONSTRAINT identifies_SUBAREA_FK FOREIGN KEY (subAreaId)
+          REFERENCES SUBAREA (subAreaId),
+        CONSTRAINT identifies_SUBJECT_FK FOREIGN KEY (subjectId)
+          REFERENCES SUBJECT (subjectId),
+        CONSTRAINT identifies_UK UNIQUE (subAreaId, subjectId)
+    );
+
+
+    CREATE TYPE stats_semester AS ENUM ('1', '2');
+    CREATE TYPE stats_semester_status AS ENUM ('AD', 'CD');
+
+    CREATE TABLE SEMESTER(
+    semesterId SERIAL NOT NULL,
+    subjectId SERIAL NOT NULL,
+    year  INT NOT NULL,
+    semester stats_semester NOT NULL,
+    status stats_semester_status,
+
+    CONSTRAINT SEMESTER_PK PRIMARY KEY (semesterId),
+    CONSTRAINT SEMESTRE_UK UNIQUE(year, semester, subjectId)
+    );
+
+    CREATE TABLE TEAM(
+    teamId SERIAL NOT NULL,
+    projectId SERIAL NOT NULL,
+    semesterId SERIAL NOT NULL,
+
+    CONSTRAINT TEAM_PK PRIMARY KEY (teamId),
+    CONSTRAINT TEAM_PROJECT_FK FOREIGN KEY (projectId)
+      REFERENCES PROJECT(projectId),
+    CONSTRAINT TEAM_SEMESTER_FK FOREIGN KEY (semesterId)
+      REFERENCES SEMESTER(semesterId) 
+    );
+
+    CREATE TABLE participe(
+    teamId SERIAL NOT NULL,
+    regNumber VARCHAR(9) NOT NULL,
+
+    CONSTRAINT participe_TEAM_FK FOREIGN KEY (teamId)
+      REFERENCES TEAM (teamId),
+    CONSTRAINT participe_STUDENT_FK FOREIGN KEY (regNumber)
+      REFERENCES STUDENT (regNumber)
+
+    );
+
+    CREATE TYPE stats_post AS ENUM ('ED', 'NT', 'DP');
+
+    CREATE TABLE POST(
+      postId SERIAL NOT NULL,
+      title VARCHAR(200) NOT NULL,
+      hypertext VARCHAR(5000) NOT NULL,
+      type stats_post NOT NULL,
+      createdAt DATE NOT NULL,
+      author VARCHAR(200) NOT NULL,
+      thumbnail BYTEA,
+
+      CONSTRAINT POST_PK PRIMARY KEY (postId)
+    );
+
+    CREATE TABLE is_registered(
+    regNumber VARCHAR(9) NOT NULL,
+    semesterId SERIAL NOT NULL,
+
+      CONSTRAINT is_registered_STUDENT_FK FOREIGN KEY (regNumber)
         REFERENCES STUDENT(regNumber),
-      CONSTRAINT participates_UK UNIQUE(regNumber, classId)
-  );
+      CONSTRAINT is_registered_SEMESTER_FK FOREIGN KEY (semesterId)
+        REFERENCES SEMESTER(semesterId)
 
-  CREATE TABLE executes (
-      regNumber VARCHAR(9) NOT NULL,
-      projectId SERIAL,
+    );
 
-      CONSTRAINT executes_STUDENT_FK FOREIGN KEY (regNumber)
-        REFERENCES STUDENT (regNumber),
-      CONSTRAINT executes_PROJECT_FK FOREIGN KEY (projectId)
-        REFERENCES PROJECT (projectId),
-      CONSTRAINT executes_UK UNIQUE (regNumber, projectId)
-  );
+    CREATE TABLE lectures(
+      regNumber VARCHAR(20) NOT NULL,
+      semesterId SERIAL NOT NULL,
 
-  CREATE TABLE identifies (
-      subAreaId SERIAL,
-      subjectId SERIAL,
-
-      CONSTRAINT identifies_SUBAREA_FK FOREIGN KEY (subAreaId)
-        REFERENCES SUBAREA (subAreaId),
-      CONSTRAINT identifies_SUBJECT_FK FOREIGN KEY (subjectId)
-        REFERENCES SUBJECT (subjectId),
-      CONSTRAINT identifies_UK UNIQUE (subAreaId, subjectId)
-  );
+      CONSTRAINT lectures_PROFESSOR_FK FOREIGN KEY (regNumber)
+        REFERENCES PROFESSOR(regNumber),
+      CONSTRAINT lectures_semesterId FOREIGN KEY (semesterId)
+        REFERENCES SEMESTER(semesterId)
+    );
 `,
 });
