ee818c8513d1344f8898cbba41aa45b47b76e717
Mexazonic <brunnohenriq697@gmail.com>
2022-04-21 17:28:03 -0300

    Modularização Keywords

--- src/controller/ProjectController.js
+++ src/controller/ProjectController.js
@@ -1,6 +1,4 @@
 const projectRepository = require('../repository/projectRepository');
-const semesterRepository = require('../repository/semesterRepository');
-const subjectRepository = require('../repository/subjectRepository');
 const keywordRepository = require('../repository/keywordRepository');
 const { simplifiedAllocation } = require('../utils/functions');
 
@@ -16,78 +14,12 @@ module.exports = {
     })
   },
 
-  getProjectData: (projectId) => {
-    return new Promise(async (resolve, reject) => {
-      try {
-        let User = null;
-        let Subject = null;
-        let Semester = null;
-        let Keywords = null;
-        const project = await projectRepository.getProjectData(projectId);
-
-        User = await projectRepository.getUserData(project.userid);
-        Keywords = await keywordRepository.getProjectKeywords(projectId);
-        if (project.subjectid) {
-          Subject = await subjectRepository.getSubject({ subjectid: project.subjectid });
-        }
-        if (project.semesterid) {
-          Semester = await semesterRepository.getSemester(project.semesterid);
-        }
-
-        resolve({ ...project, User, Subject, Semester, Keywords });
-      } catch (error) {
-        reject(error);
-      }
-    });
-  },
-
-  updateProject: (project) => {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const mainKeyword = project.keywords.find((k) => k.main)?.keywordid;
-        const subjectid = await projectRepository.getSubjectByKeyword(mainKeyword);
-
-        await projectRepository.removeProjectKeywords(project.projectid);
-        await projectRepository.addProjectKeywords(project.projectid, project.keywords);
-
-        await projectRepository.updateProject({ ...project, subjectid });
-        resolve({ status: 'OK' });
-      } catch (error) {
-        reject(error);
-      }
-    })
-  },
-
-  evaluateProject: (payload) => {
-    return new Promise((resolve, reject) => {
-      projectRepository.evaluateProject(payload).then((response) => {
-        resolve(response)
-      }).catch((e) => reject(e));
-    });
-  },
-
-  reallocateProject: (payload) => {
-    return new Promise((resolve, reject) => {
-      projectRepository.reallocateProject(payload).then((response) => {
-        resolve(response)
-      }).catch((e) => reject(e));
-    });
-  },
-
   getUserProposals: (user) => {
     return new Promise((resolve, reject) => {
       projectRepository.getUserProposals(user).then((response) => {
         resolve(response);
       }).catch((error) => { reject(error) });
     })
-  },
-
-  deleteProject: (projectId) => {
-    return new Promise((resolve, reject) => {
-      projectRepository.deleteProject(projectId).then((response) => {
-        resolve(response);
-      }).catch((error) => { reject(error) });
-    });
   },
 
   addFile: (file) => {
@@ -100,14 +32,15 @@ module.exports = {
     })
   },
 
-  getKeywordsAvailbleToProject: () => {
+  deleteProject: (projectId) => {
     return new Promise((resolve, reject) => {
-      keywordRepository.getKeywordsAvailbleToProject().then((response) => {
+      try {
+        const response = projectRepository.deleteProject(projectId);
         resolve(response);
-      }).catch((error) => { reject(error) });
-    })
+      } catch (e) { reject(e); }
+      resolve();
+    });
   },
-
   getKnowledgeAreas: () => {
     return new Promise((resolve, reject) => {
       try {
@@ -117,7 +50,6 @@ module.exports = {
       resolve();
     });
   },
-
   getKeywords: () => {
     return new Promise((resolve, reject) => {
       try {
@@ -169,7 +101,7 @@ module.exports = {
     });
   },
 
-  updateSubjectKeyword: (keywordid, subjectid) => {
+  updateKeyword: (keywordid, subjectid) => {
     return new Promise((resolve, reject) => {
       try {
         resolve(keywordRepository.updateSubjectKeyword(keywordid, subjectid));
