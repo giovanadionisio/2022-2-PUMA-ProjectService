6d5fc4de3a4b145917b7e4852d74dc6dd42d58b6
Mexazonic <brunnohenriq697@gmail.com>
2022-05-06 02:43:27 -0300

    Adição de ambiente de configuração de homologação

--- src/config/environment.config.js
+++ src/config/environment.config.js
@@ -5,19 +5,19 @@ module.exports = {
   configEnv: () => {
     console.log('ENVIRONMENT:', process.env.ENVIRONMENT);
     if (process.env.ENVIRONMENT === 'dev') {
-      global.URL_API = http://${process.env.IP_ADDRESS}:3004;
+      global.URL_API = `http://${process.env.IP_ADDRESS}:3004`;
       global.SECRET = process.env.SECRET;
       global.DB_URL = process.env.DB_URL;
     } else if (process.env.ENVIRONMENT === 'test') {
-      global.URL_API = http://${process.env.IP_ADDRESS}:3004;
+      global.URL_API = `http://${process.env.IP_ADDRESS}:3004`;
       global.SECRET = process.env.SECRET;
       global.DB_URL = process.env.DB_URL_TEST;
     } else if (process.env.ENVIRONMENT === 'hom') {
-      global.URL_API = http://${process.env.IP_ADDRESS}:3004;
+      global.URL_API = `http://${process.env.IP_ADDRESS}:3004`;
       global.SECRET = process.env.SECRET;
       global.DB_URL = process.env.DB_URL;
     } else if (process.env.ENVIRONMENT === 'prod') {
-      global.URL_API = http://${process.env.IP_ADDRESS}:3004;
+      global.URL_API = `http://${process.env.IP_ADDRESS}:3004`;
       global.SECRET = process.env.SECRET_PROD;
       global.DB_URL = process.env.DB_URL;
     }
