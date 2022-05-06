c9ef1c65907948ccbf3180a8d16e072abe0a4653
Mexazonic <brunnohenriq697@gmail.com>
2022-05-06 02:25:42 -0300

    Adição de ambiente pra homologação - Project Service

--- src/config/environment.config.js
+++ src/config/environment.config.js
@@ -5,20 +5,21 @@ module.exports = {
   configEnv: () => {
     console.log('ENVIRONMENT:', process.env.ENVIRONMENT);
     if (process.env.ENVIRONMENT === 'dev') {
-      global.URL_API = `http://${process.env.IP_ADDRESS}:3004`;
+      global.URL_API = http://${process.env.IP_ADDRESS}:3004;
       global.SECRET = process.env.SECRET;
       global.DB_URL = process.env.DB_URL;
     } else if (process.env.ENVIRONMENT === 'test') {
-      global.URL_API = `http://${process.env.IP_ADDRESS}:3004`;
+      global.URL_API = http://${process.env.IP_ADDRESS}:3004;
       global.SECRET = process.env.SECRET;
       global.DB_URL = process.env.DB_URL_TEST;
+    } else if (process.env.ENVIRONMENT === 'hom') {
+      global.URL_API = http://${process.env.IP_ADDRESS}:3004;
+      global.SECRET = process.env.SECRET;
+      global.DB_URL = process.env.DB_URL;
     } else if (process.env.ENVIRONMENT === 'prod') {
-      global.URL_API = `http://${process.env.IP_ADDRESS}:3004`;
+      global.URL_API = http://${process.env.IP_ADDRESS}:3004;
       global.SECRET = process.env.SECRET_PROD;
       global.DB_URL = process.env.DB_URL;
-      console.log('ADRESS', global.URL_API);
-      console.log('SECRET', global.SECRET);
-      console.log('DB', global.DB_URL);
     }
   },
 };
